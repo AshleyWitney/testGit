781a06787023f12eac64bb9d12b410c2ec5045b3
Witney KONATE <witneykonate@gmail.com>
2023-01-11 08:48:51 +0100

    test deuxieùme commit
    - une description du travail
    - une autre description
    - testeu
    
    Ashley

--- javascript.js
+++ javascript.js
@@ -5,3 +5,15 @@ let b = 45;
 
 b+= a;
 console.log("Le résultat de a + b = " +b);
+
+
+
+let aPerson = { 
+	name: 'Jean',
+	age: 35,
+	gender: 'masculin',
+	interest: ['musiquetet', 'Jeux videos'],
+};
+
+aPerson.sayHello = function(){console.log('Bonjour! je suis' + this.name + '.');};
+aPerson.sayHello();
